b27ee1f8cce87e3849213de826e7d1b8ca3c020b
abelsan <abel@mit.edu>
2016-11-06 19:30:57 -0500

    dynamic js

---
+++ js/footer.js
@@ -0,0 +1,10 @@
+
+kogs.footer = function(element){
+    var html = '';
+    html = '<footer class="footer">' +
+            '<div class="container">' +
+                '<p class="text-muted">Learning Board</p>' +
+            '</div>' +
+        '</footer>';
+    document.getElementById('pagefooter').innerHTML = html;
+};
