9b42ca3f166927e4fb058912d2f5d1d9e6f55a45
abelsan <abel@mit.edu>
2016-11-14 22:26:46 -0500

    image picker & multiple tag selector

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ kogs.footer = function(element){
     var html = '';
     html = '<footer class="footer">' +
             '<div class="container">' +
-                '<p class="text-muted">Learning Board</p>' +
+                '<p class="text-muted">KOGS</p>' +
             '</div>' +
         '</footer>';
     document.getElementById('pagefooter').innerHTML = html;
